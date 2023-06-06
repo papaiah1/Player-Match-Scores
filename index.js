cefae6db0c8472ed627740050c4f321ce8ab3400
papaiah1 <jangampapaiah05@gmail.com>
2023-06-06 09:58:37 +0000

    Player Match Scores

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="bgContainer">
+    <h1 className="heading">Congratulations</h1>
+    <div className="cardContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="cardHeading">Kiran V</h1>
+      <p className="cardPara">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
